7934f5e0b7d36da771699ef5d66c343007aad304
alstdu <lexi.dugo@gmail.com>
2023-11-25 18:25:31 -0800

    Add API call utility functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,28 @@
 console.log( 'hello world' );
 
-const getSpells = async () => {
-    const response = await fetch( 'https://www.dnd5eapi.co/api/spells' );
-    const spells = await response.json();
-    return spells;
+const API_BASE = 'https://www.dnd5eapi.co';
+
+const getJsonFromUrl = async ( url ) => {
+    const response = await fetch( url );
+    const parsed = await response.json();
+    return parsed;
+};
+
+const getSpellsList = async () => {
+    // this endpoint returns an object with a "count" property and a "results" property
+    // we just want the results so get that. We can use .length if we want the count
+    return ( await getJsonFromUrl( `${API_BASE}/api/spells` ) ).results;
+};
+
+const getSpellFromUrl = async ( spellPath ) => {
+    return await getJsonFromUrl( `${API_BASE}${spellPath}` );
 };
 
 ( async () => {
-    console.log( await getSpells() );
+    const spellsList = await getSpellsList();
+    console.log( spellsList );
+
+    const firstSpell = spellsList[0];
+    console.log( firstSpell );
+    console.log( await getSpellFromUrl( firstSpell.url ) );
 } )();
