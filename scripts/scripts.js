f194ecff37f202e89ef5f43a34e083183dad2bf5
alstdu <lexi.dugo@gmail.com>
2023-11-25 17:47:03 -0800

    Fetch list of spells

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,11 @@
 console.log( 'hello world' );
+
+const getSpells = async () => {
+    const response = await fetch( 'https://www.dnd5eapi.co/api/spells' );
+    const spells = await response.json();
+    return spells;
+};
+
+( async () => {
+    console.log( await getSpells() );
+} )();
