5c2456e43c34d71f10a191ed077675851e4aa9ea
alstdu <lexi.dugo@gmail.com>
2023-11-25 21:44:00 -0800

    Populate list from API
    - added debug mode to protect against spamming API
    - melt brains with async foreach map list.js add nonsense

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 console.log( 'hello world' );
 
 const API_BASE = 'https://www.dnd5eapi.co';
+const DEBUG = true; // TODO: set to false before submit
 
 const getJsonFromUrl = async ( url ) => {
     const response = await fetch( url );
@@ -19,10 +20,52 @@ const getSpellFromUrl = async ( spellPath ) => {
 };
 
 ( async () => {
-    const spellsList = await getSpellsList();
-    console.log( spellsList );
+    let spellsList = await getSpellsList();
 
-    const firstSpell = spellsList[0];
-    console.log( firstSpell );
-    console.log( await getSpellFromUrl( firstSpell.url ) );
+    if ( DEBUG ) {
+        const seed = Math.random();
+        const len = 10;
+        const offset = Math.floor( seed * ( spellsList.length - len ) );
+        spellsList = spellsList.slice( offset, len + offset );
+    }
+
+    const options = {
+        valueNames: [
+            'spell-item-name',
+            'spell-item-level',
+            'spell-item-time',
+            'spell-item-school',
+            'spell-item-concentration',
+            'spell-item-range',
+        ],
+        item: `<tr id="spell-item">
+                <td class="spell-item-name"></td>
+                <td class="spell-item-level"></td>
+                <td class="spell-item-time"></td>
+                <td class="spell-item-school"></td>
+                <td class="spell-item-concentration"></td>
+                <td class="spell-item-range"></td>
+            </tr>`,
+    };
+
+    // initialize the spells table with an empty array because we're going to fill it later async
+    const spellsTable = new List( 'spells-table', options, [] );
+
+    spellsList.forEach( async ( spellSummary ) => {
+        const url = spellSummary?.url;
+        if ( !url ) {
+            return;
+        }
+        const spell = await getSpellFromUrl( url );
+        const formattedSpell = {
+            'spell-item-name': spell.name,
+            'spell-item-level': spell.level,
+            'spell-item-time': spell.casting_time,
+            'spell-item-school': spell.school.name,
+            'spell-item-concentration': spell.concentration ? 'C' : '-',
+            'spell-item-range': spell.range,
+        };
+
+        spellsTable.add( formattedSpell );
+    } );
 } )();
