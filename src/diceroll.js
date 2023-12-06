c7ced334cdff22cf5d782af168bbb43b302b8d61
alstdu <lexi.dugo@gmail.com>
2023-12-05 21:27:50 -0800

    Fix dice color issue

--- src/diceroll.js
+++ src/diceroll.js
@@ -11,7 +11,7 @@ diceBox.init().then( () => {
     diceBox.roll( {
         qty: 1, // optional - the number of dice to be rolled. Defaults to 1
         sides: 20, // the type of die to be rolled. Either a number such as 20
-        themeColor: '#fff', // optional - HEX value for the theme's material color
+        themeColor: '#ffffff', // optional - HEX value for the theme's material color
     } );
 } );
 
