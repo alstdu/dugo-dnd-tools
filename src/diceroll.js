98c958118b874d304a738d5e115820ddc3f50310
alstdu <lexi.dugo@gmail.com>
2023-12-06 19:33:00 -0800

    Add dice roll button and functionality

--- src/diceroll.js
+++ src/diceroll.js
@@ -1,18 +1,26 @@
 import DiceBox from '@3d-dice/dice-box';
 
-console.log( DiceBox );
-
 const diceBox = new DiceBox( '#dice-box', {
     assetPath: '/public/assets/',
     scale: 8,
 } );
 
-diceBox.init().then( () => {
-    diceBox.roll( {
+const diceBoxInit = diceBox.init();
+
+// document.querySelector( '#roll-button' ).addEventListener( 'click', diceBox.roll );
+// document.querySelector( '#roll-button' ).addEventListener( 'click', diceBox.roll( {
+//     qty: 1, // optional - the number of dice to be rolled. Defaults to 1
+//     sides: 20, // the type of die to be rolled. Either a number such as 20
+//     themeColor: '#ffffff', // optional - HEX value for the theme's material color
+// } ) );
+document.querySelector( '.roll-button' )?.addEventListener( 'click', async () => {
+    await diceBoxInit;
+    const result = await diceBox.roll( {
         qty: 1, // optional - the number of dice to be rolled. Defaults to 1
         sides: 20, // the type of die to be rolled. Either a number such as 20
         themeColor: '#ffffff', // optional - HEX value for the theme's material color
     } );
+    console.log( result );
 } );
 
 // set up the canvas and states for the gradients
