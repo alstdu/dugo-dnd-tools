2eee076d45481423e528c5c90e3b3e19417ea162
alstdu <lexi.dugo@gmail.com>
2024-12-03 10:29:50 -0800

    Add spell loading functionality with pagination and styling for 'Load More' button

--- scripts/spells.js
+++ scripts/spells.js
@@ -1,6 +1,3 @@
-// does the code even work? ...okay good
-// console.log( 'hello world' );
-
 // set up the canvas and states for the gradients
 //      these states will trigger as the point count goes up
 
@@ -29,6 +26,10 @@
 const API_BASE = 'https://www.dnd5eapi.co';
 const DEBUG = false; // TODO: set to false before submit
 const DEBUG_AMOUNT = 5;
+
+const SPELLS_PER_PAGE = 20;
+let currentPage = 0;
+let cachedSpells = new Map();
 
 const getJsonFromUrl = async ( url ) => {
     const response = await fetch( url );
@@ -250,176 +251,198 @@ const granimInstance = new Granim( {
     // initialize the spells table with an empty array because we're going to fill it later async
     const spellsTable = new List( 'listjs-container', options, [] );
 
-    const populatedList = Promise.all( spellsList.map( async ( spellSummary ) => {
-        const url = spellSummary?.url;
-        if ( !url ) {
-            return;
+    const loadSpellsBatch = async (startIndex) => {
+        const endIndex = startIndex + SPELLS_PER_PAGE;
+        const batchSpells = spellsList.slice(startIndex, endIndex);
+        
+        await Promise.all(batchSpells.map(async (spellSummary) => {
+            if (!cachedSpells.has(spellSummary.index)) {
+                const spell = await getSpellFromUrl(spellSummary.url);
+                cachedSpells.set(spellSummary.index, spell);
+            }
+
+            const spell = cachedSpells.get(spellSummary.index);
+            const formattedSpell = {
+                'spell-item-name': spell.name,
+                'spell-item-level': spell.level > 0 ? spell.level : 'Cantrip',
+                'spell-item-time': spell.casting_time,
+                'spell-item-school': spell.school.name,
+                'spell-item-concentration': spell.concentration ? 'C' : '-',
+                'spell-item-range': spell.range,
+            };
+
+            // double destructuring ooo fancy
+            // get the "elm" property from the first object in array
+            const [{elm}] = spellsTable.add( formattedSpell );
+
+            // add an extra td for the button that will allow TAB to be used
+            // to expand the rows
+            const expandCell = document.createElement( 'td' );
+            elm.insertBefore( expandCell, elm.firstChild );
+            // add a button inside of the new td we created
+            const expandButton = document.createElement( 'button' );
+            expandButton.innerText = '+';
+            expandButton.className = 'expand-button';
+            expandCell.appendChild( expandButton );
+
+            // TODO: evaluate if the following tabIndex logic is actually a good idea
+            elm.tabIndex = 0;
+            // TODO: can we set this to -1 if we let enter/space expand the row while tabbed?
+            expandButton.tabIndex = 0;
+
+            // configure detail
+            const detailElm = document.createElement( 'td' );
+            detailElm.style.display = 'none';
+            detailElm.style.flexBasis = '100%';
+            detailElm.style.minHeight = '300px';
+            detailElm.classList.add( 'spell-item-detail' );
+
+            console.log( spell );
+
+            // append inner stuff to detail
+            const levelElm = document.createElement( 'p' );
+            const levelSpanElm = document.createElement( 'span' );
+            levelSpanElm.style.fontWeight = 'bold';
+            levelSpanElm.innerText = 'Level: ';
+            levelElm.appendChild( levelSpanElm );
+            // if level is greater than 0, display the level. Else cantrip
+            levelElm.innerHTML += spell.level > 0 ? spell.level : 'Cantrip';
+            detailElm.appendChild( levelElm );
+
+            const timeElm = document.createElement( 'p' );
+            const timeSpanElm = document.createElement( 'span' );
+            timeSpanElm.style.fontWeight = 'bold';
+            timeSpanElm.innerText = 'Casting Time: ';
+            timeElm.appendChild( timeSpanElm );
+            timeElm.innerHTML += spell.casting_time;
+            detailElm.appendChild( timeElm );
+
+            const schoolElm = document.createElement( 'p' );
+            const schoolSpanElm = document.createElement( 'span' );
+            schoolSpanElm.style.fontWeight = 'bold';
+            schoolSpanElm.innerText = 'School: ';
+            schoolElm.appendChild( schoolSpanElm );
+            schoolElm.innerHTML += spell.school.name;
+            detailElm.appendChild( schoolElm );
+
+            // create a p tag with a span inside
+            const durationElm = document.createElement( 'p' );
+            const durationSpanElm = document.createElement( 'span' );
+            durationSpanElm.style.fontWeight = 'bold';
+            durationSpanElm.innerText = 'Duration: ';
+            // durationSpanElm needs to be appended first so that it shows up on the left
+            durationElm.appendChild( durationSpanElm );
+            // now that we appended the span we can ADD the text
+            // note: we are adding the text with += not overwriting it with =, lest we nuke the poor span :(
+            // note also that: we are using innerHTML instead of innerText, else...
+            // the text from the span stays but the span itself dissapears...
+            // which is very fun to debug as a beginner
+            durationElm.innerHTML += spell.duration;
+            detailElm.appendChild( durationElm );
+
+            const rangeElm = document.createElement( 'p' );
+            const rangeTitleSpan = document.createElement( 'span' );
+            rangeTitleSpan.innerText = 'Range: ';
+            rangeElm.appendChild( rangeTitleSpan );
+            rangeTitleSpan.style.fontWeight = 'bold';
+            rangeElm.innerHTML += spell.range;
+            detailElm.appendChild( rangeElm );
+
+            const concentrationElm = document.createElement( 'p' );
+            const concentrationSpan = document.createElement( 'span' );
+            concentrationSpan.innerText = 'Concentration: ';
+            concentrationElm.appendChild( concentrationSpan );
+            concentrationSpan.style.fontWeight = 'bold';
+            concentrationElm.innerHTML += spell.concentration;
+            detailElm.appendChild( concentrationElm );
+
+            const componentsElm = document.createElement( 'p' );
+            const componentsSpanElm = document.createElement( 'span' );
+            componentsSpanElm.style.fontWeight = 'bold';
+            componentsSpanElm.innerText = 'Components: ';
+            componentsElm.appendChild( componentsSpanElm );
+            // using join to add the comma and space to each item in the list
+            componentsElm.innerHTML += spell.components.join( ', ' );
+            // if the spell uses a material, then we will display the material text
+            if ( spell.material ) {
+                componentsElm.innerHTML += ' (' + spell.material + ')';
+            }
+            detailElm.appendChild( componentsElm );
+
+            // spell.desc is a list of strings that represent paragraphs
+            // note: the spell "confusion" is an interesting test case here...
+            spell.desc.forEach( ( paragraphText ) => {
+                const descriptionElm = document.createElement( 'p' );
+                descriptionElm.innerText = paragraphText;
+                detailElm.appendChild( descriptionElm );
+            } );
+
+            // if the spell has higher_level, it will display. else it won't show up
+            if ( spell.higher_level.length > 0 ) {
+                const atHigherLevelsElm = document.createElement( 'p' );
+                atHigherLevelsElm.innerText = 'At higher levels:';
+                atHigherLevelsElm.style.fontWeight = 'bold';
+                detailElm.appendChild( atHigherLevelsElm );
+            }
+
+            // spell.higher_level is a list of strings that represent paragraphs
+            spell.higher_level.forEach( ( paragraphText ) => {
+                const higherLevelElm = document.createElement( 'p' );
+                higherLevelElm.innerText = paragraphText;
+                detailElm.appendChild( higherLevelElm );
+            } );
+
+            // append detail
+            elm.appendChild( detailElm );
+
+            elm.addEventListener( 'click', () => {
+                const isCurrentlyExpanded = elm.getAttribute( 'aria-expanded' ) === 'true';
+                elm.setAttribute( 'aria-expanded', !isCurrentlyExpanded );
+                // if it's currently expanded, get rid of it - else make it show.
+                detailElm.style.display = isCurrentlyExpanded ? 'none' : 'table-cell';
+            } );
+
+            const setBg = () => granimInstance.changeState( spell.school.index );
+            const unsetBg = ( e ) => {
+                // TODO: use e.target to keep bg if we have a row open
+                granimInstance.changeState( 'default-state' );
+            };
+            // add highlight on both mouseover and focus
+            //     adding on focus makes the effect work with tabIndex
+            elm.addEventListener( 'mouseover', setBg );
+            elm.addEventListener( 'focus', setBg );
+            expandButton.addEventListener( 'focus', setBg );
+            // remove highlight when mouseout and on blur
+            //     which is apparently when it's not in focus
+            //     which makes sense I guess but... why not focus lost ?
+            elm.addEventListener( 'mouseout', unsetBg );
+            elm.addEventListener( 'blur', unsetBg );
+            expandButton.addEventListener( 'blur', unsetBg );
+        }));
+
+        // Add Load More button if needed
+        if (endIndex < spellsList.length && !document.querySelector('#load-more')) {
+            const loadMoreButton = document.createElement('button');
+            loadMoreButton.id = 'load-more';
+            loadMoreButton.innerText = 'Load More Spells';
+            loadMoreButton.addEventListener('click', () => {
+                currentPage++;
+                loadSpellsBatch(currentPage * SPELLS_PER_PAGE);
+            });
+            document.querySelector('.table-wrapper').appendChild(loadMoreButton);
+        } else if (endIndex >= spellsList.length) {
+            const loadMore = document.querySelector('#load-more');
+            if (loadMore) loadMore.remove();
         }
-        const spell = await getSpellFromUrl( url );
-        const formattedSpell = {
-            'spell-item-name': spell.name,
-            'spell-item-level': spell.level > 0 ? spell.level : 'Cantrip',
-            'spell-item-time': spell.casting_time,
-            'spell-item-school': spell.school.name,
-            'spell-item-concentration': spell.concentration ? 'C' : '-',
-            'spell-item-range': spell.range,
-        };
+    };
 
-        // double destructuring ooo fancy
-        // get the "elm" property from the first object in array
-        const [{elm}] = spellsTable.add( formattedSpell );
+    // Load initial batch
+    await loadSpellsBatch(0);
 
-        // add an extra td for the button that will allow TAB to be used
-        // to expand the rows
-        const expandCell = document.createElement( 'td' );
-        elm.insertBefore( expandCell, elm.firstChild );
-        // add a button inside of the new td we created
-        const expandButton = document.createElement( 'button' );
-        expandButton.innerText = '+';
-        expandButton.className = 'expand-button';
-        expandCell.appendChild( expandButton );
-
-        // TODO: evaluate if the following tabIndex logic is actually a good idea
-        elm.tabIndex = 0;
-        // TODO: can we set this to -1 if we let enter/space expand the row while tabbed?
-        expandButton.tabIndex = 0;
-
-        // configure detail
-        const detailElm = document.createElement( 'td' );
-        detailElm.style.display = 'none';
-        detailElm.style.flexBasis = '100%';
-        detailElm.style.minHeight = '300px';
-        detailElm.classList.add( 'spell-item-detail' );
-
-        console.log( spell );
-
-        // append inner stuff to detail
-        const levelElm = document.createElement( 'p' );
-        const levelSpanElm = document.createElement( 'span' );
-        levelSpanElm.style.fontWeight = 'bold';
-        levelSpanElm.innerText = 'Level: ';
-        levelElm.appendChild( levelSpanElm );
-        // if level is greater than 0, display the level. Else cantrip
-        levelElm.innerHTML += spell.level > 0 ? spell.level : 'Cantrip';
-        detailElm.appendChild( levelElm );
-
-        const timeElm = document.createElement( 'p' );
-        const timeSpanElm = document.createElement( 'span' );
-        timeSpanElm.style.fontWeight = 'bold';
-        timeSpanElm.innerText = 'Casting Time: ';
-        timeElm.appendChild( timeSpanElm );
-        timeElm.innerHTML += spell.casting_time;
-        detailElm.appendChild( timeElm );
-
-        const schoolElm = document.createElement( 'p' );
-        const schoolSpanElm = document.createElement( 'span' );
-        schoolSpanElm.style.fontWeight = 'bold';
-        schoolSpanElm.innerText = 'School: ';
-        schoolElm.appendChild( schoolSpanElm );
-        schoolElm.innerHTML += spell.school.name;
-        detailElm.appendChild( schoolElm );
-
-        // create a p tag with a span inside
-        const durationElm = document.createElement( 'p' );
-        const durationSpanElm = document.createElement( 'span' );
-        durationSpanElm.style.fontWeight = 'bold';
-        durationSpanElm.innerText = 'Duration: ';
-        // durationSpanElm needs to be appended first so that it shows up on the left
-        durationElm.appendChild( durationSpanElm );
-        // now that we appended the span we can ADD the text
-        // note: we are adding the text with += not overwriting it with =, lest we nuke the poor span :(
-        // note also that: we are using innerHTML instead of innerText, else...
-        // the text from the span stays but the span itself dissapears...
-        // which is very fun to debug as a beginner
-        durationElm.innerHTML += spell.duration;
-        detailElm.appendChild( durationElm );
-
-        const rangeElm = document.createElement( 'p' );
-        const rangeTitleSpan = document.createElement( 'span' );
-        rangeTitleSpan.innerText = 'Range: ';
-        rangeElm.appendChild( rangeTitleSpan );
-        rangeTitleSpan.style.fontWeight = 'bold';
-        rangeElm.innerHTML += spell.range;
-        detailElm.appendChild( rangeElm );
-
-        const concentrationElm = document.createElement( 'p' );
-        const concentrationSpan = document.createElement( 'span' );
-        concentrationSpan.innerText = 'Concentration: ';
-        concentrationElm.appendChild( concentrationSpan );
-        concentrationSpan.style.fontWeight = 'bold';
-        concentrationElm.innerHTML += spell.concentration;
-        detailElm.appendChild( concentrationElm );
-
-        const componentsElm = document.createElement( 'p' );
-        const componentsSpanElm = document.createElement( 'span' );
-        componentsSpanElm.style.fontWeight = 'bold';
-        componentsSpanElm.innerText = 'Components: ';
-        componentsElm.appendChild( componentsSpanElm );
-        // using join to add the comma and space to each item in the list
-        componentsElm.innerHTML += spell.components.join( ', ' );
-        // if the spell uses a material, then we will display the material text
-        if ( spell.material ) {
-            componentsElm.innerHTML += ' (' + spell.material + ')';
-        }
-        detailElm.appendChild( componentsElm );
-
-        // spell.desc is a list of strings that represent paragraphs
-        // note: the spell "confusion" is an interesting test case here...
-        spell.desc.forEach( ( paragraphText ) => {
-            const descriptionElm = document.createElement( 'p' );
-            descriptionElm.innerText = paragraphText;
-            detailElm.appendChild( descriptionElm );
-        } );
-
-        // if the spell has higher_level, it will display. else it won't show up
-        if ( spell.higher_level.length > 0 ) {
-            const atHigherLevelsElm = document.createElement( 'p' );
-            atHigherLevelsElm.innerText = 'At higher levels:';
-            atHigherLevelsElm.style.fontWeight = 'bold';
-            detailElm.appendChild( atHigherLevelsElm );
-        }
-
-        // spell.higher_level is a list of strings that represent paragraphs
-        spell.higher_level.forEach( ( paragraphText ) => {
-            const higherLevelElm = document.createElement( 'p' );
-            higherLevelElm.innerText = paragraphText;
-            detailElm.appendChild( higherLevelElm );
-        } );
-
-        // append detail
-        elm.appendChild( detailElm );
-
-        elm.addEventListener( 'click', () => {
-            const isCurrentlyExpanded = elm.getAttribute( 'aria-expanded' ) === 'true';
-            elm.setAttribute( 'aria-expanded', !isCurrentlyExpanded );
-            // if it's currently expanded, get rid of it - else make it show.
-            detailElm.style.display = isCurrentlyExpanded ? 'none' : 'table-cell';
-        } );
-
-        const setBg = () => granimInstance.changeState( spell.school.index );
-        const unsetBg = ( e ) => {
-            // TODO: use e.target to keep bg if we have a row open
-            granimInstance.changeState( 'default-state' );
-        };
-        // add highlight on both mouseover and focus
-        //     adding on focus makes the effect work with tabIndex
-        elm.addEventListener( 'mouseover', setBg );
-        elm.addEventListener( 'focus', setBg );
-        expandButton.addEventListener( 'focus', setBg );
-        // remove highlight when mouseout and on blur
-        //     which is apparently when it's not in focus
-        //     which makes sense I guess but... why not focus lost ?
-        elm.addEventListener( 'mouseout', unsetBg );
-        elm.addEventListener( 'blur', unsetBg );
-        expandButton.addEventListener( 'blur', unsetBg );
-    } ) );
-
-    await populatedList;
-
-    document.querySelector( '.preload' ).classList.remove( 'preload' );
-    const overlay = document.querySelector( '#loading-overlay' );
+    document.querySelector('.preload').classList.remove('preload');
+    const overlay = document.querySelector('#loading-overlay');
     overlay.style.opacity = '0%';
-    setTimeout( () => {
+    setTimeout(() => {
         overlay.style.display = 'none';
-    }, 1000 );
-} )();
+    }, 1000);
+})();
